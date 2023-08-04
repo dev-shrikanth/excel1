295266c904af1c87fd2016cf57d865bdcca10852
dev-shrikanth <dev@shrikanth.net>
2023-08-04 12:00:24 -0700

    Named Range Data Setting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import openpyxl
-from openpyxl import load_workbook
+from openpyxl.workbook.defined_name import DefinedName
+
 
 def get_api_response(api_url):
     response = requests.get(api_url)
@@ -10,10 +11,10 @@ def get_api_response(api_url):
         print(f"Failed to get API response. Status Code: {response.status_code}")
         return None
 
-def write_json_list_to_excel(json_list, output_filename):
+
+def write_json_list_to_excel(json_list, output_filename, named_range):
     workbook = openpyxl.Workbook()
-    sheet = workbook.create_sheet("Blogs")
-    sheet.title = "Blogs List"
+    sheet = workbook.active
 
     # Write headers in the worksheet
     headers = list(json_list[0].keys())
@@ -25,16 +26,26 @@ def write_json_list_to_excel(json_list, output_filename):
         for col_num, header in enumerate(headers, start=1):
             sheet.cell(row=row_num, column=col_num, value=item.get(header, ""))
 
+    # Create a named range
+    named_range_ref = f'{sheet.title}!$A$1:${openpyxl.utils.get_column_letter(len(headers))}${len(json_list) + 1}'
+    # named_range_obj = DefinedName(name=named_range, localSheetId=0, formula=named_range_ref)
+    named_range_obj = DefinedName(name=named_range, localSheetId=0, attr_text=named_range_ref)
+    # workbook.defined_names.definedName.append(named_range_obj)
+    workbook.defined_names.add(named_range_obj)
+
     # Save the Excel file
     workbook.save(output_filename)
     print(f"JSON data successfully written to {output_filename}")
+    print(f"Named range '{named_range}' created for the data.")
+
 
 if __name__ == "__main__":
-    api_url = input("Enter the API URL: ")
+    api_url = "https://jsonplaceholder.typicode.com/posts"
     json_response = get_api_response(api_url)
 
     if json_response and isinstance(json_response, list):
-        output_filename = input("Enter the output Excel filename (e.g., output.xlsx): ")
-        write_json_list_to_excel(json_response, output_filename)
+        output_filename = "output.xlsx"
+        named_range = input("Enter the name for the named range: ")
+        write_json_list_to_excel(json_response, output_filename, named_range)
     else:
         print("Invalid API response. The API should return a list of dictionaries.")
